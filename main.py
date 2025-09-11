ac33eb2255d0720b3fd9a7d37d4eaccb7f8624d0
Jordanna <jordanabruna90@gmail.com>
2025-09-11 20:01:51 -0300

    cria classes controller, model, repository, service

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 
-# 1. Cria a instância principal da nossa aplicação
+from user import user_controller
+
+#Cria a instância principal da nossa aplicação
 app = FastAPI(
     title="API do Meu Projeto",
     version="0.1.0"
 )
 
-# 2. Criando o primeiro endpoint
+app.include_router(user_controller.router)
+
 @app.get("/")
 def read_root():
-    return {"message": "Bem-vindo à API do nosso projeto!"}
+    return {"message": "API está no ar!"}
 
-# 3. Endpoint com Parâmetro de Rota (Path Parameter)
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-# 4. Código para rodar o servidor
+#Código para rodar o servidor
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
