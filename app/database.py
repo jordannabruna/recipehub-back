3619616d8a1a7095f99f3c1878a81d275c165962
Jordanna Ribeiro <62577429+jordannabruna@users.noreply.github.com>
2025-10-09 19:54:26 -0300

    configura banco de dados

--- app/database.py
+++ app/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Configure as variáveis de ambiente ou substitua pelos seus dados
+APP_PROFILE =   os.getenv("APP_PROFILE", "DEV")
+
+if APP_PROFILE == "DEV":
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./recipehub.db"
+else:
+    SQLALCHEMY_DATABASE_URL = os.getenv("APP_PROFILE")
+
 DB_USER = os.getenv('POSTGRES_USER', 'postgresql')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', '12345678')
 DB_HOST = os.getenv('POSTGRES_HOST', 'localhost')
@@ -12,7 +18,6 @@ DB_NAME = os.getenv('POSTGRES_DB', 'recipe_hub')
 
 # SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Configuração para SQLite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./recipehub.db"
 
 engine = create_engine(
